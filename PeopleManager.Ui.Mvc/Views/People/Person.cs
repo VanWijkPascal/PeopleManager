b15d53f8545300b864cf41b21f1079262dd470be
VivesPascalVanWijk <pascal.vanwijk@student.vives.be>
2026-02-24 09:11:48 +0100

    Add "Person.cs"

---
+++ PeopleManager.Ui.Mvc/Views/People/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace PeopleManager.Ui.Mvc.Views.People
+{
+    public class Person
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Email { get; set; }
+    }
+}
